6b63b8c4e36354a9c72db1a763444f65f04c04d3
Mohammed Yahya <mdyahya2093@gmail.com>
2026-07-20 14:00:28 +0530

    Time: 1 ms (61.90%) | Memory: 42.4 MB (53.22%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int reverseBits(int n) {
+        
+        int res = 0;
+
+        for(int i=0; i<32; i++)
+        {
+            int bit = n&1;
+            n>>=1;
+            res<<=1;
+            res |=bit;
+        }
+
+        return res;
+
+    }
+}
