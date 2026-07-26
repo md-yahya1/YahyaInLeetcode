acf5ca41c99e51677d5878df6972270da885d864
Mohammed Yahya <mdyahya2093@gmail.com>
2026-07-26 18:19:18 +0530

    Time: 1 ms (99.73%) | Memory: 47.5 MB (8.50%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int jump(int[] nums) {
+        
+        int left = 0;
+        int right = 0;
+        int jump = 0;
+
+        while(right < nums.length-1)
+        {
+            int far = 0;
+            for(int i=left; i<=right; i++)
+            {
+                far = Math.max(far, nums[i] + i);
+
+            }
+
+            left = right + 1;
+            right = far;
+            jump++;
+        }
+
+        return jump;
+    }
+}
