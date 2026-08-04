765abd49e96b001d42cff783edb2d90a6ca45e90
Mohammed Yahya <mdyahya2093@gmail.com>
2026-08-04 21:30:55 +0530

    Time: 9 ms (7.73%) | Memory: 46.9 MB (14.93%) - LeetSync

---
+++ 51-n-queens/n-queens.java
@@ -0,0 +1,105 @@
+class Solution {
+
+    public boolean isSafe(int row, int col, List<StringBuilder> chessboard, int n) { // Queen lies in
+                                                                                     // Same Column or Row ?
+        for (int i = 0; i < n; i++) {
+            if (chessboard.get(row).charAt(i) == 'Q' || chessboard.get(i).charAt(col) == 'Q') {
+                return false;
+            }
+        }
+
+        // Queen lies in Diagonals ?
+        // UP LEFT
+
+        int r = row;
+        int c = col;
+
+        while (r >= 0 && c >= 0) {
+            if (chessboard.get(r).charAt(c) == 'Q') {
+                return false;
+            }
+
+            r--;
+            c--;
+        }
+
+        // UP RIGHT
+        r = row;
+        c = col;
+
+        while (r >= 0 && c < n) {
+            if (chessboard.get(r).charAt(c) == 'Q')
+                return false;
+
+            r--;
+            c++;
+        }
+
+        r = row;
+        c = col;
+
+        while (r < n && c >= 0) {
+            if (chessboard.get(r).charAt(c) == 'Q')
+                return false;
+
+            r++;
+            c--;
+        }
+
+        // DOWN RIGHT
+        r = row;
+        c = col;
+        while (r < n && c < n) {
+            if (chessboard.get(r).charAt(c) == 'Q') {
+                return false;
+            }
+
+            r++;
+            c++;
+        }
+
+        // ALL false No Queens then
+        return true;
+    }
+
+    public void recursion(int row, int n, List<StringBuilder> chessboard, List<List<String>> answer) {
+        if (row == n) {
+            List<String> current = new ArrayList<>();
+
+            for (int i = 0; i < chessboard.size(); i++) {
+                current.add(chessboard.get(i).toString());
+            }
+            answer.add(current);
+            return;
+        }
+
+        for (int j = 0; j < n; j++) {
+            if (isSafe(row, j, chessboard, n)) {
+                //Set the Queen
+                chessboard.get(row).setCharAt(j, 'Q');
+                recursion(row + 1, n, chessboard, answer);
+
+                //Remove the Queen
+                chessboard.get(row).setCharAt(j, '.');
+            }
+
+        }
+    }
+
+    public List<List<String>> solveNQueens(int n) {
+
+        List<List<String>> answer = new ArrayList<>();
+        List<StringBuilder> chessboard = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            StringBuilder row = new StringBuilder();
+            for (int j = 0; j < n; j++) {
+                row.append(".");
+            }
+            chessboard.add(row);
+        }
+
+        recursion(0, n, chessboard, answer);
+
+        return answer;
+    }
+}
