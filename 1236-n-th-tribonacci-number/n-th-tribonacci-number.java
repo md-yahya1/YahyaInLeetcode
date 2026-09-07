0f2869499db7ffab51e5a830df875b318e90cd00
Mohammed Yahya <mdyahya2093@gmail.com>
2026-09-07 21:05:42 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (16.09%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -10,17 +10,19 @@ class Solution {
             return 1;
         }
 
-        int[] dp = new int[n+1];
-        
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 1;
+        int a = 0;
+        int b = 1;
+        int c = 1;
 
         for(int i=3; i<=n; i++)
         {
-            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+            int newNo = a + b + c;
+
+            a = b;
+            b = c;
+            c = newNo;
         }
-        return dp[n];
+        return c;
     }
 
 }
