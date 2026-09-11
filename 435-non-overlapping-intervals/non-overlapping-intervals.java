6100b1d42042850bc0221c67442af0bfc7641a81
Mohammed Yahya <mdyahya2093@gmail.com>
2026-09-11 19:38:21 +0530

    Time: 48 ms (41.44%) | Memory: 116.1 MB (9.11%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/non-overlapping-intervals.java
@@ -3,20 +3,19 @@ class Solution {
         
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
 
-        int notOverLapping = 1;
+        int count = 1;
         int freeTime = intervals[0][1];
 
         for(int i=1; i<intervals.length; i++)
         {
-            int start = intervals[i][0];
-            if(freeTime <= start)
+            int startTime = intervals[i][0];
+            if(freeTime <= startTime)
             {
+                count++;
                 freeTime = intervals[i][1];
-                notOverLapping++;
             }
         }
 
-        return intervals.length - notOverLapping;
-
+        return intervals.length - count;
     }
 }
